60251e35f1d6e51235fdd9ee8fcd09b1078eadae
010pixel <010pixel@gmail.com>
2023-10-03 18:58:43 +0800

    Added 3 more extensions

--- src/shared/data/index.tsx
+++ src/shared/data/index.tsx
@@ -218,6 +218,54 @@ const exntensions: BrowserExtension[] = [
 			},
 		],
 	},
+	{
+		id: 10,
+		slug: 'gmail-new-email-to-all-re',
+		name: 'Gmail new email to all recipients by 010Pixel',
+		colorText: 'white-text text-lighten-2',
+		colorBg: 'red lighten-2',
+		description: 'A plugin for gmail to automatically create email with all recipients of an email.',
+		logo: 'https://lh3.googleusercontent.com/2H7HBI02LqQbHNazZCZa8ugtBBN8-rvrk9h73pqhNiK6FgOl3aJEhT3xeuxKU8I_EvdyJ_c60kPuhse57uPpY1pt=w128-h128-e365-rj-sc0x00ffffff',
+		banner: '',
+		links: [
+			{
+				browser: 'chrome',
+				url: 'https://chrome.google.com/webstore/detail/gmail-new-email-to-all-re/cbnkjkdjjhmbabdmnlppnpefolaakhba',
+			},
+		],
+	},
+	{
+		id: 11,
+		slug: 'gmail-automatic-bcc-me-by',
+		name: 'Gmail automatic BCC me by 010Pixel',
+		colorText: 'white-text text-lighten-2',
+		colorBg: 'red lighten-2',
+		description: "A plugin for gmail to automatically BCC sender's email.",
+		logo: 'https://lh3.googleusercontent.com/Oxd3EUySYS0_fDR15rnKRjCkugyRuaDUKAtgd8cEuGMfHOsi5h0n81RR_fJU225dbYKxx8IzSdsvfz0KI1f2ImFTPw=w128-h128-e365-rj-sc0x00ffffff',
+		banner: '',
+		links: [
+			{
+				browser: 'chrome',
+				url: 'https://chrome.google.com/webstore/detail/gmail-automatic-bcc-me-by/cdbbhmihggfhmhbbmabpkpflahdeodgo',
+			},
+		],
+	},
+	{
+		id: 12,
+		slug: 'live-bookmarks',
+		name: 'Live Bookmarks',
+		colorText: 'white-text text-lighten-2',
+		colorBg: 'red lighten-2',
+		description: 'A bookmark viewer to see all your bookmarks with screenshot of the webpage.',
+		logo: 'https://lh3.googleusercontent.com/QETho-jUl6M2M4l470Y-ewbJJ8YWwPDEVSAz8SjT10W7-X9BD2WAgCIWovuVjJOk4GKHmR9MhZb5hCHIEFZhCf9x=w128-h128-e365-rj-sc0x00ffffff',
+		banner: '',
+		links: [
+			{
+				browser: 'chrome',
+				url: 'https://chrome.google.com/webstore/detail/live-bookmarks/ihfolgbjkckclahmpoejnoglilaeenji',
+			},
+		],
+	},
 ];
 
 export default exntensions;
